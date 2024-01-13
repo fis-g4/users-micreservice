1529efbeeab63e0b1a1affcdfa952376aee9430a
Alex-GF <yalejandro9@gmail.com>
2024-01-13 11:21:45 +0100

    CI/CD: FIXED SUBNETWORKS ISSUE IN CLOUD NAT DEPLOYMENT

--- terraform/gce.tf
+++ terraform/gce.tf
@@ -12,7 +12,7 @@ resource "google_compute_instance" "users_service_instance" {
   }
 
   network_interface {
-    network = google_compute_subnetwork.user_service_subnetwork.name
+    network = google_compute_network.fis_g4_network_cd.name
 
     access_config {
     }
